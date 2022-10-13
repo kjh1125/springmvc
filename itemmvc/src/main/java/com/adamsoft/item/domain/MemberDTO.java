bf04e273329ec098e01705e7027cc4699d302d16
tjoeun <tjoeun@host.docker.internal>
2022-10-13 18:21:10 +0900

    origin

---
+++ itemmvc/src/main/java/com/adamsoft/item/domain/MemberDTO.java
@@ -0,0 +1,36 @@
+package com.adamsoft.item.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberDTO {
+	private String name;
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "MemberDTO [name=" + name + ", password=" + password + "]";
+	}
+	
+	
+	
+}
---
+++ itemmvc/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>처음 만든 결과 페이지</title>
+</head>
+<body>
+	<p>나는 aespa 와 뉴진스를 좋아하는 사람입니다.</p>
+</body>
+</html>
